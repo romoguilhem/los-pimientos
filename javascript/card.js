1ab490b88bd45a0258ba015cfda59091550961a0
Guilhem Romo <guilhem.romo@orange.fr>
2023-09-05 18:57:25 +0200

    taff sur les cards pour modifier l'effet, plus genre modal

--- javascript/card.js
+++ javascript/card.js
@@ -21,11 +21,45 @@ cards.forEach(card => {
       navbar.classList.add("hidden");
     }, 500);
 
-    //on ajoute un listenner pour la close avec echap ou clic à côté
+    //on ajoute un listenner pour la close avec echap ou clic à côté (manquant)
     window.addEventListener('keydown', (event) => {
       if (event.key === 'Escape') {
         card.classList.remove('clicked');
       }
     });
   });
+});
+
+// Gérer les cards comme des modals
+const modalCard = document.querySelector('.modal-card');
+const modalContainer = document.querySelector('.modal-container');
+const modalContent = document.getElementById('modal-card-content');
+const closeButton = document.querySelector('.close-card');
+
+// Ajout d'un gestionnaire d'événements à chaque carte
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+      // Récupérer le contenu de la carte cliquée
+      modalContent.innerHTML = card.innerHTML;
+
+      // Afficher la modale
+      modalCard.style.display = 'flex';
+  });
+});
+
+// Gestionnaire d'événement pour fermer la modale en cliquant en dehors de celle-ci
+closeButton.addEventListener('click', () => {
+  modalCard.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === modalCard) {
+      modalCard.style.display = 'none';
+  }
+});
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    modalCard.style.display = 'none';
+  }
 });
